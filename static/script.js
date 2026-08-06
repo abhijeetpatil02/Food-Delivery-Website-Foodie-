a9948f3b07c3e63be6bd5c973a0b4525400894d4
abhijeetpatil02 <patilabhijeet261@gmail.com>
2026-08-06 20:59:01 +0530

    changig in menu

--- static/script.js
+++ static/script.js
@@ -11,6 +11,7 @@
             
 };*/
 
+
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".login").onclick = (event) => {
         event.preventDefault(); // Stop the form from submitting
