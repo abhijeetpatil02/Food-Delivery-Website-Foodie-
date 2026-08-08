a075fd6c19666441983dfc22cd51fcd04ebfd474
abhijeetpatil02 <patilabhijeet261@gmail.com>
2026-08-08 22:30:27 +0530

    changing script

--- static/script.js
+++ static/script.js
@@ -12,7 +12,6 @@
 };*/
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".login").onclick = (event) => {
         event.preventDefault(); // Stop the form from submitting
