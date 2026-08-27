39f91f51f446a92e3f9c872e3fd36d0b019b97ca
abhijeetpatil02 <patilabhijeet261@gmail.com>
2026-08-27 18:13:03 +0530

    add function

--- static/script.js
+++ static/script.js
@@ -13,7 +13,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".login").onclick = (event) => {
-        event.preventDefault(); // Stop the form from submiting
+        event.preventDefault(); // Stop the  from submiting
 
         const username = document.getElementById("username");
         const password = document.getElementById("password");
